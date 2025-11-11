19196ad3c811b36016cb79431913f402bf882cf8
lukacavic <luka.cavic@gmail.com>
2025-11-11 08:23:23 +0100

    Change dashboard title

--- app/Filament/App/Pages/Dashboard.php
+++ app/Filament/App/Pages/Dashboard.php
@@ -5,16 +5,20 @@ namespace App\Filament\App\Pages;
 use BackedEnum;
 use Filament\Pages\Page;
 use Filament\Support\Icons\Heroicon;
+use Illuminate\Contracts\Support\Htmlable;
 
 class Dashboard extends Page
 {
     protected string $view = 'filament.app.pages.dashboard';
-
-    protected static ?string $title = 'Dashboard';
 
     protected static ?int $navigationSort = -2;
 
     protected static ?string $navigationLabel = 'Dashboard';
 
     protected static string|BackedEnum|null $navigationIcon = Heroicon::Home;
+
+    public function getTitle(): string|Htmlable
+    {
+        return 'Hi, ' . auth()->user()->first_name;
+    }
 }
